3d83a6caa927d79b3d4a7fc6d703d35676811290
Doris Behrendt <doris-behrendt@gymnasium-marktbreit.de>
2015-12-19 12:37:34 +0100

    ohne transistor, ohne hintergrundwissen nur 16 s"aulen und 4 ebenenpins

---
+++ vorversuch-4x4x4/vorversuch-4x4x4.ino
@@ -0,0 +1,25 @@
+
+int c[16] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, A4, A5}; 
+int z[4] = {A0, A1, A2, A3}; 
+//vorversuch
+void setup() 
+{ 
+  for(int i=0; i<16; i++) 
+  {pinMode(c[i], OUTPUT);} 
+  for(int i=0; i<4; i++) 
+  {pinMode(z[i], OUTPUT);} 
+}
+void loop() 
+{ 
+  for(int i=0; i<16;i++)
+  {digitalWrite(c[i], HIGH);
+  }
+  for(int i=0; i<4;i++)
+  {digitalWrite(z[i], LOW);
+  delay(1000);
+  }
+  for(int i=0; i<4;i++)
+  {
+  digitalWrite(z[i], HIGH);
+  }
+} 
