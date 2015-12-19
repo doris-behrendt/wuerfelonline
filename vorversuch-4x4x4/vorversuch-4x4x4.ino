2252699918e5320f99bba795a0589916818d4d4c
Doris Behrendt <doris-behrendt@gymnasium-marktbreit.de>
2015-12-19 12:57:15 +0100

    scheint iwie invertiert zu sein; habe w"urfel mit transistoren; und wenn ich -- so dachte ich -- auf leuchten gesetzt hatte, ist es aus, und umgekehrt, wenn ich auf "aus" gesetzt hatte, war es an

--- vorversuch-4x4x4/vorversuch-4x4x4.ino
+++ vorversuch-4x4x4/vorversuch-4x4x4.ino
@@ -1,25 +1,29 @@
-
-int c[16] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, A4, A5}; 
-int z[4] = {A0, A1, A2, A3}; 
+//s f"ur s"aule, e f"ur ebene
+int s[16] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, A4, A5}; 
+int e[4] = {A0, A1, A2, A3}; 
 //vorversuch
 void setup() 
 { 
   for(int i=0; i<16; i++) 
-  {pinMode(c[i], OUTPUT);} 
+  {pinMode(s[i], OUTPUT);} 
   for(int i=0; i<4; i++) 
-  {pinMode(z[i], OUTPUT);} 
+  {pinMode(e[i], OUTPUT);} 
 }
+//damit ne diode (i,j) aus [0,15]x[0,3] leuchtet, muss s"aule i an Plus und ebene j an Minus!
+//so ist es zumindest beim behrendtschen prototyp 
 void loop() 
 { 
-  for(int i=0; i<16;i++)
-  {digitalWrite(c[i], HIGH);
-  }
   for(int i=0; i<4;i++)
-  {digitalWrite(z[i], LOW);
+  {digitalWrite(e[i], LOW);
   delay(1000);
   }
+  for(int i=0; i<16;i++)
+  {digitalWrite(s[i], HIGH);
+  }
+  
   for(int i=0; i<4;i++)
   {
-  digitalWrite(z[i], HIGH);
+  digitalWrite(e[i], HIGH);
   }
+  delay(2000);
 } 
